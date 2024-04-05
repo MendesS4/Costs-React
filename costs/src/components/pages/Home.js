989988597729452e3706625605cbc0557a7b20e0
Samuel Mendes <mendessamuel1@outlook.com>
2024-04-04 23:56:46 -0300

    css home

--- costs/src/components/pages/Home.js
+++ costs/src/components/pages/Home.js
@@ -1,9 +1,15 @@
+import styles from './Home.module.css'
+import savings from '../../img/savings.svg'
+
 function Home() {
 
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
+       <section className = {styles.home_container}>
+        <h1>Bem-vindo ao <spam>Costs</spam></h1>
+        <p>Comece a gerenciar os seus projetos agora mesmo!</p>
+        <a href="/">Criar Projeto</a>
+        <img src={savings} alt ="Costs" />
+       </section>
     )
 
 }
