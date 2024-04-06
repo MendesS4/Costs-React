9e0017603bfef0a698a423c9317291b6e8f4fc0e
Samuel Mendes <mendessamuel1@outlook.com>
2024-04-06 18:19:38 -0300

    backend connection

--- costs/src/components/form/Select.js
+++ costs/src/components/form/Select.js
@@ -6,6 +6,9 @@ function Select({ text, name, options, handleOnChange, value }) {
             <label htmlFor={name}>{text}:</label>
             <select name={name} id={name}>
                 <option>Selectione uma opção</option>
+                {options.map((option) => (
+                    <option value = {option.id} key={option.id}> {option.name} </option>
+                ))}
             </select>
         </div>
     )
