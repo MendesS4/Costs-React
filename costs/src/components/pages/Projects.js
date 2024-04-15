08006f87e2ff4e46e5dfa78072a1c9e958a8be96
Samuel Mendes <mendessamuel1@outlook.com>
2024-04-14 21:16:30 -0300

    remove projects

--- costs/src/components/pages/Projects.js
+++ costs/src/components/pages/Projects.js
@@ -14,6 +14,7 @@ function Projects() {
 
     const [projects, setProjects] = useState([])
     const [removeLoading, setRemoveLoading] = useState(false)
+    const [projectMessage, setProjectMessage] = useState('')
 
     const location = useLocation()
     let message = "";
@@ -22,22 +23,37 @@ function Projects() {
     }
 
     useEffect(() => {
-      setTimeout(() => {
-        fetch('http://localhost:5000/projects', {
-            method: 'GET',
-            headers: {
-                'Content-type': 'application/json',
-            },
-        })
-            .then((resp) => resp.json())
-            .then((data) => {
-                console.log(data)
-                setProjects(data)
-                setRemoveLoading(true)
+        setTimeout(() => {
+            fetch('http://localhost:5000/projects', {
+                method: 'GET',
+                headers: {
+                    'Content-type': 'application/json',
+                },
             })
-            .catch((err) => console.log(err))
-      }, 300)
+                .then((resp) => resp.json())
+                .then((data) => {
+                    console.log(data)
+                    setProjects(data)
+                    setRemoveLoading(true)
+                })
+                .catch((err) => console.log(err))
+        }, 300)
     }, [])
+
+    function removeProject(id) {
+        fetch(`http://localhost:5000/projects/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        }).then(resp => resp.json())
+        .then(() => {
+            setProjects(projects.filter((project) => project.id !== id))
+            setProjectMessage('Projeto removido com sucesso.')
+        })
+        .catch(err => console.log(err))
+
+    }
 
     return (
         <div className={styles.project_container}>
@@ -46,6 +62,8 @@ function Projects() {
                 <LinkButton to="/newproject" text="Criar Projeto" />
             </div>
             {message && <Message type="success" msg={message} />}
+            {projectMessage && <Message type="success" msg={projectMessage} />}
+
             <Container customClass="start">
                 {projects.length > 0 &&
                     projects.map((project) => (
@@ -55,6 +73,7 @@ function Projects() {
                             budget={project.budget}
                             category={project.category.name}
                             key={project.id}
+                            handleRemove={removeProject}
                         />
                     ))
                 }
--- costs/src/components/project/ProjectCard.js
+++ costs/src/components/project/ProjectCard.js
@@ -4,7 +4,13 @@ import styles from './ProjectCard.module.css'
 
 import { BsPencil, BsFillTrashFill } from 'react-icons/bs'
 
-function ProjectCard({ id, name, budget, category, hgandleRemove }) {
+function ProjectCard({ id, name, budget, category, handleRemove }) {
+
+    const remove = (e) => {
+        e.preventDefault()
+        handleRemove(id)
+    }
+
     return (
         <div className={styles.project_card}>
             <h4>{name}</h4>
@@ -18,7 +24,7 @@ function ProjectCard({ id, name, budget, category, hgandleRemove }) {
                 <Link to="/">
                     <BsPencil /> Editar
                 </Link>
-                <button>
+                <button onClick={remove}>
                     <BsFillTrashFill /> Excluir
                 </button>
             </div>
