ef132e072768a369580154ff77a80aae88f35ba5
Samuel Mendes <mendessamuel1@outlook.com>
2024-04-17 23:23:29 -0300

    page project view

--- costs/src/App.js
+++ costs/src/App.js
@@ -3,6 +3,8 @@ import Home from './components/pages/Home'
 import Company from './components/pages/Company'
 import Contact from './components/pages/Contact'
 import Projects from './components/pages/Projects'
+import Project from './components/pages/Project'
+
 import NewProject from './components/pages/NewProject'
 import NavBar from './components/layouts/NavBar'
 import Footer from './components/layouts/Footer'
@@ -20,7 +22,8 @@ function App() {
           <Route path="/projects" element={< Projects />} />
           <Route path="/company" element={< Company />} />
           <Route path="/contact" element={< Contact />} />
-          <Route path="newproject" element={< NewProject />} />
+          <Route path="/newproject" element={< NewProject />} />
+          <Route path="/project/:id" element={< Project />} />
         </Routes>
       </Container>
       <Footer/>
--- costs/src/components/pages/Home.js
+++ costs/src/components/pages/Home.js
@@ -6,7 +6,7 @@ function Home() {
 
     return (
        <section className = {styles.home_container}>
-        <h1>Bem-vindo ao <spam>Costs</spam></h1>
+        <h1>Bem-vindo ao <span>Costs</span></h1>
         <p>Comece a gerenciar os seus projetos agora mesmo!</p>
         <LinkButton to="/newproject" text = "Criar Projeto"/>
         <img src={savings} alt ="Costs" />
